609a5209a359b29fa25b8e7a799c62d3fc148526
Sorin Chis <kish.sorin@yahoo.com>
2019-06-30 20:16:34 +0300

    server succesfully working

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const expresss = require("express");
+const globalMiddleware = require("../middleware/globalMiddleware");
+
+//routes here
+//const userRoutes = require("../routes/user-routes");
+//const authRoutes = require("../routes/auth-routes");
+
+
+//server
+const server = expresss();
+globalMiddleware(server);
+
+//use the router
+// server.use(userRoutes);
+// server.use(authRoutes);
+
+
+
+//export server
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config(); //for loading from .env file
+
+const server = require("./api/server");
+
+server.get("/", (req, res) => {
+  res.send("<h1>Unsilenceddddddd</h1>");
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server running on http://localhost:${port}  ***\n`);
+});

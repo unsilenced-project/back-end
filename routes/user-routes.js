9f04827878210386c3806f914f74644736068479
Sorin Chis <kish.sorin@yahoo.com>
2019-07-01 10:48:37 +0300

    added endpoint for deleting a user

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -14,15 +14,34 @@ router.get("/users", restricted, async (req, res, next) => {
 router.get("/users/:id", restricted, async (req, res, next) => {
   const { id } = req.params;
 
-  try{
-    const user = await dbHelpers.getUserbyId(id)
-    if(user){
-      res.status(200).json(user)
-    }else{
-      res.status(404).json({message: `The specified ID not found`});
+  try {
+    const user = await dbHelpers.getUserbyId(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: `The specified ID not found` });
     }
-  }catch(error){
-    res.status(500).json({error:"Error trying to get a user by Id"})
+  } catch (error) {
+    res.status(500).json({ error: "Error trying to get a user by Id" });
+  }
+});
+
+router.delete("/user/:id", restricted, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await dbHelpers.remove(id);
+    if (result === 1) {
+      res.status(200).json({
+        message: "User was delete Succesfully"
+      });
+    } else {
+      res.status(404).json({ message: "User was not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "There was a error trying to delete the user" });
   }
 });
 

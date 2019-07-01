5555d6db3471b4913ce5075ea6194fbff45b0c5e
Sorin Chis <kish.sorin@yahoo.com>
2019-07-01 11:06:46 +0300

    added endpoint for udate a user

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -45,4 +45,23 @@ router.delete("/users/:id", restricted, async (req, res) => {
   }
 });
 
+router.put("/users/:id", restricted, async (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+
+  try {
+    const result = await dbHelpers.update(id, user);
+    if (result === 1) {
+      res.status(200).json({
+        updateID: id,
+        message: `User: ${user.username} Update succesfully`
+      });
+    } else {
+      res.status(404).json({ message: "User was not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error trying to update a user" });
+  }
+});
+
 module.exports = router;
